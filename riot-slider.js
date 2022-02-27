a19a9e7a53f92d15b5c08041bbc8a3be473be2db
adam k <krayvo@gmail.com>
2022-02-27 14:11:48 -0700

    css, js, and html to display and slide images complete

--- riot-slider.js
+++ riot-slider.js
@@ -1,327 +1,135 @@
 class RiotSlider {
-  constructor (elemId) {
+  constructor () {
     this.elems = {
       main: null,
-      buttons: null,
-      outerCon: null,
-      innerCon: null,
-      slideLinks: null,
-      buttonCon: null,
-      slidesCon: null,
-      play: null,
-      stop: null,
-      prev: null,
-      next: null
+      //window: null,
+      //buttons: null,
+      slidesOuter: null,
+      slidesInner: null,
+      slides: null
+      //slideList: null,
+      //buttonCon: null,
+      //slidesCon: null,
+      //play: null,
+      //stop: null,
+      //prev: null,
+      //next: null
     }
-    this.slides = []
     this.slideNum = 1
+    this.slideCount = 0
+    this.slideInterval = null
+    this.intervalIsSet = false
+    this.intervalTime = 3000
+    /*    this.slides = []
     this.slideInterval = null
     this.intervalIsSet = false
     this.imageWidth = 0
-    this.intervalTime = 3000
+    
     this.origImgWidth = 1920
     this.origImgHeight = 1080
 
-    this.minSlideWidth = null;
-    this.minSlideHeight = null;
-    this.minSlideWidth = null;
-    this.maxSlideHeight = null;
+    this.sliderWidth = null
 
-    this.mainElementId = elemId;
+
+
+    this.images = []*/
   }
 
-  load () {
-    this.elems.main = $('#' + this.mainElementId)
+  load (elemId) {
+    this.elems.main = $('#' + elemId)
     if (this.elems.main.length < 1) {
-      console.log('Riot Slider not loaded. Element ID not found:' + this.mainElementId);
+      console.log('Riot Slider not loaded. Element ID not found:' + elemId)
       return false
     }
 
-    this.elems.main.addClass('riotSlider');
+    let html = this.elems.main.html()
+    html = $('<div></div>')
+      .addClass('slidesInner')
+      .html(html)
+    html = $('<div></div>')
+      .addClass('slidesOuter')
+      .html(html)
+    this.elems.main.html(html)
 
-    /*this.elems.innerCon = this.elems.main.find('.slidesConInner')
-    this.elems.slideElems = this.elems.innerCon.find('.slide')
+    this.elems.slidesInner = this.elems.main.find('.slidesInner')
+    this.elems.slides = this.elems.slidesInner.find('.slide')
 
-    this.elems.buttonCon = this.elems.main.find('.buttonCon')
-    this.elems.slideLinks = this.elems.buttonCon.find('.slideLink')
-
-    this.elems.play = this.elems.buttonCon.find('.slideLinkPlay')
-    this.elems.stop = this.elems.buttonCon.find('.slideLinkStop')
-    this.elems.prev = this.elems.buttonCon.find('.slideLinkPrev')
-    this.elems.next = this.elems.buttonCon.find('.slideLinkNext')
-    this.elems.next = this.elems.buttonCon.find('.slideLinkNext')
-    this.elems.slidesCon = this.elems.main.find('.slidesCon')
-
-    this.elems.slideElems.css('display', 'inline-block')
-    this.elems.buttonCon.css('display', 'block')
+    this.slideCount = this.elems.slides.length
 
     this.setWidth()
 
     this.bindAll()
 
+    
+    let slideWidth = 100 / this.slideCount + '%'
+    this.elems.slides.css('width', slideWidth)
+
+    this.elems.main.addClass('isLoaded')
+
     this.goToSlide()
-    this.startInterval()*/
-
-    this.elems.main.addClass('isLoaded');
+    this.startInterval()
   }
-}
-/*
+
+  setWidth () {
+    this.sliderWidth = this.elems.main.width()
+    let sliderInnerWidth = this.sliderWidth * this.slideCount
+    this.elems.slidesInner.css('width', sliderInnerWidth + 'px')
+    console.log('Riot Slider width changed. New Width = ' + sliderInnerWidth)
+  }
+
+  /*bindAll () {
+    $(window).on('resize', {instance: this}, function (event) {
+      event.data.instance.setWidth()
+    })
+  }*/
+
   bindAll () {
-
-    $(window).resize(function () {
-      RiotSlider.setWidth()
-    })
-
-    this.elems.slideLinks.on('click', function () {
-      RiotSlider.stopInterval()
-      var num = $(this)
-        .html()
-        .replace('slideLink', '')
-        RiotSlider.slideNum = num
-      //console.log(num);
-      RiotSlider.goToSlide()
-    })
-
-    this.elems.play.on('click', function () {
-      RiotSlider.incrementNumber(1)
-      RiotSlider.goToSlide()
-      RiotSlider.startInterval()
-    })
-
-    this.elems.stop.on('click', function () {
-      RiotSlider.elem.stop.addClass('active')
-      setInterval(function () {
-        RiotSlider.elem.stop.removeClass('active')
-      }, 1000)
-      RiotSlider.stopInterval()
-    })
-
-    this.elems.prev.on('click', function () {
-      RiotSlider.elem.prev.addClass('active')
-      setInterval(function () {
-        RiotSlider.elem.prev.removeClass('active')
-      }, 1000)
-      RiotSlider.stopInterval()
-      RiotSlider.incrementNumber(-1)
-      RiotSlider.goToSlide()
-    })
-
-    this.elems.next.on('click', function () {
-      RiotSlider.elem.next.addClass('active')
-      setInterval(function () {
-        RiotSlider.elem.next.removeClass('active')
-      }, 1000)
-      RiotSlider.stopInterval()
-      RiotSlider.incrementNumber(1)
-      RiotSlider.goToSlide()
-    })
+    $(window).on('resize', this.setWidth)
   }
 
-  startInterval () {
-    //console.log('startInterval');
-    this.stopInterval()
-    this.elems.play.addClass('active')
-    this.intervalIsSet = true
-    this.slideInterval = setInterval(function () {
-      //console.log('Change Slide');
-      RiotSlider.incrementNumber(1)
-      RiotSlider.goToSlide()
-      //console.log(riotSlider.slideNum);
-    }, this.intervalTime)
+  goToSlide () {
+    var val = (this.slideNum - 1) * this.sliderWidth
+    this.elems.slidesInner.css('margin-left', '-' + val + 'px')
+    //this.elem.slideLinks.removeClass('active');
+    //this.elem.slideLinks.filter('.slideLink'+this.slideNum).addClass('active');
   }
 
   stopInterval () {
     if (this.intervalIsSet) {
-      this.elems.play.removeClass('active')
-      clearInterval(this.slideInterval)
+      //this.elem.play.removeClass('active');
+      //clearInterval(this.slideInterval);
       this.intervalIsSet = false
     }
   }
 
-  incrementNumber (increment) {
+  slideIncrementAndGo () {
+    this.incrementSlideNumber()
+    this.goToSlide()
+  }
+
+  incrementSlideNumber (increment) {
+    if (typeof increment === 'undefined') {
+      increment = 1
+    }
     this.slideNum = this.slideNum + increment
     if (this.slideNum < 1) {
-      this.slideNum = this.elems.slideElems.length
+      this.slideNum = this.slideCount
     }
-    if (this.slideNum > this.elems.slideElems.length) {
+    if (this.slideNum > this.slideCount) {
       this.slideNum = 1
     }
   }
 
-  setWidth () {
-    var w = this.elems.main.width()
-
-    if (w == this.imageWidth) {
-      return false
-    }
-
-    this.imageWidth = w
-    var h = (this.imageWidth / this.origImgWidth) * this.origImgHeight
-    this.elems.innerCon.css('width', w + 'px').css('height', h + 'px')
-    this.elems.slidesCon.css('height', h + 'px')
-  }
-
-  goToSlide () {
-    var val = (this.slideNum - 1) * this.imageWidth
-    this.elems.innerCon.css('left', '-' + val + 'px')
-    this.elems.slideLinks.removeClass('active')
-    this.elems.slideLinks
-      .filter('.slideLink' + this.slideNum)
-      .addClass('active')
+  startInterval () {
+    console.log(this);
+    //console.log('startInterval');
+    this.stopInterval()
+    //this.elem.play.addClass('active');
+    this.intervalIsSet = true
+    let a = '123'
+    this.slideInterval = setInterval(
+      this.slideIncrementAndGo.bind(this),
+      this.intervalTime
+    )
   }
 }
-*/
-/*
-var riotSlider = {
-	elem: {
-		main:null, 
-		buttons:null, 
-		outerCon:null, 
-		innerCon:null,
-		slideLinks:null, 
-		buttonCon:null,
-		slidesCon:null,
-		play:null,
-		stop:null,
-		prev:null,
-		next:null
-	},
-	slideNum:1,
-	slideInterval:null,
-	intervalIsSet:false,
-	imageWidth:0,
-	intervalTime: 3000,
-	origImgWidth:1920,
-	origImgHeight:1080,
-};
-
-
-
-riotSlider.init = function(elemId) {
-    this.elems.main = $('#'+elemId);
-	this.elems.innerCon = this.elems.main.find('.slidesConInner');
-	this.elems.slideElems = this.elems.innerCon.find('.slide');
-	
-	this.elems.buttonCon = this.elems.main.find('.buttonCon');
-	this.elems.slideLinks = this.elems.buttonCon.find('.slideLink');
-	
-	this.elems.play = this.elems.buttonCon.find('.slideLinkPlay');
-	this.elems.stop = this.elems.buttonCon.find('.slideLinkStop');
-	this.elems.prev = this.elems.buttonCon.find('.slideLinkPrev');
-	this.elems.next = this.elems.buttonCon.find('.slideLinkNext');
-	this.elems.next = this.elems.buttonCon.find('.slideLinkNext');
-	this.elems.slidesCon = this.elems.main.find('.slidesCon');
-
-	this.elems.slideElems.css('display', 'inline-block');
-	this.elems.buttonCon.css('display', 'block');
-	
-	this.setWidth();
-	
-	$(window).resize(function() {
-        riotSlider.setWidth();
-    });
-	
-	this.bindAll();
-	
-	this.goToSlide();
-	this.startInterval();
-};
-
-riotSlider.bindAll = function() {
-	this.elems.slideLinks.on( "click", function() {
-		riotSlider.stopInterval();
-		var num = $( this ).html().replace('slideLink', '');
-		riotSlider.slideNum = num;
-		//console.log(num);
-		riotSlider.goToSlide();
-	});
-	
-	this.elems.play.on( "click", function() {
-		riotSlider.incrementNumber(1);
-		riotSlider.goToSlide();
-		riotSlider.startInterval();
-	});
-	
-	this.elems.stop.on( "click", function() {
-		riotSlider.elem.stop.addClass('active');
-		setInterval(function(){ 
-			riotSlider.elem.stop.removeClass('active');
-		},1000);
-		riotSlider.stopInterval();
-	});
-	
-	this.elems.prev.on( "click", function() {
-		riotSlider.elem.prev.addClass('active');
-		setInterval(function(){ 
-			riotSlider.elem.prev.removeClass('active');
-		},1000);
-		riotSlider.stopInterval();
-		riotSlider.incrementNumber(-1);
-		riotSlider.goToSlide();
-	});
-	
-	this.elems.next.on( "click", function() {
-		riotSlider.elem.next.addClass('active');
-		setInterval(function(){ 
-			riotSlider.elem.next.removeClass('active');
-		},1000);
-		riotSlider.stopInterval();
-		riotSlider.incrementNumber(1);
-		riotSlider.goToSlide();
-	});
-};
-
-riotSlider.startInterval = function() {
-	//console.log('startInterval');
-	this.stopInterval();
-	this.elems.play.addClass('active');
-	this.intervalIsSet = true;
-	this.slideInterval = setInterval(function(){ 
-			//console.log('Change Slide');
-			riotSlider.incrementNumber(1);
-			riotSlider.goToSlide();
-			//console.log(riotSlider.slideNum);
-		},
-		this.intervalTime);
-};
-
-riotSlider.stopInterval = function() {
-    if (this.intervalIsSet) {
-		this.elems.play.removeClass('active');
-		clearInterval(this.slideInterval);
-		this.intervalIsSet = false;
-	}
-};
-
-riotSlider.incrementNumber = function(increment) {
-	this.slideNum = this.slideNum+increment;
-	if (this.slideNum < 1) 
-	{
-		this.slideNum = this.elems.slideElems.length;
-	}
-	if (this.slideNum > this.elems.slideElems.length)
-	{
-		this.slideNum = 1;
-	}
-};
-
-riotSlider.setWidth = function() {
-	var w = this.elems.main.width();
-
-	if (w==this.imageWidth)
-	{
-		return false;
-	}
-
-	this.imageWidth = w;
-	var h = this.imageWidth/this.origImgWidth*this.origImgHeight;
-    this.elems.innerCon.css('width', w+"px").css('height', h+'px');
-	this.elems.slidesCon.css('height', h+'px');
-};
-
-riotSlider.goToSlide = function() {
-	var val = ((this.slideNum-1) * this.imageWidth);
-	this.elems.innerCon.css('left', '-'+val+'px');
-	this.elems.slideLinks.removeClass('active');
-	this.elems.slideLinks.filter('.slideLink'+this.slideNum).addClass('active');
-};*/
